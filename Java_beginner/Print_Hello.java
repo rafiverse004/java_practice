22c792a1c1aa63e10aef94d2bdc3a53b533bcfc8
Md Rafiqul Islam <rafiverse004@gmail.com>
2025-10-15 00:00:14 +0600

    Trying

---
+++ Java_beginner/Print_Hello.java
@@ -0,0 +1,7 @@
+public class Print_Hello 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Java!");
+    }
+}
---
+++ Java_beginner/Variables_Example.java
@@ -0,0 +1,13 @@
+public class Variables_Example 
+{
+    public static void main(String[] args)
+    {
+        int age = 21;
+        double height = 5.7;
+        String name = "Rafik";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet.");
+    }
+}
